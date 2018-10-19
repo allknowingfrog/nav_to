3c368ff34acec6c792560c6897958dfb7f8d3e06
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2018-10-19 10:50:52 -0500

    Strip whitespace from :class

--- app/helpers/nav_to/helper.rb
+++ app/helpers/nav_to/helper.rb
@@ -5,6 +5,7 @@ module NavTo
       options[:class] << ' nav-item nav-link'
       active_tabs = instance_variable_get("@#{NavTo.hash_name}") || {}
       options[:class] << ' active' if active_tabs[nav] == tab
+      options[:class].strip!
       label = options.key?(:label) ? options[:label] : tab.to_s.titleize
       link_to label, path, options
     end
